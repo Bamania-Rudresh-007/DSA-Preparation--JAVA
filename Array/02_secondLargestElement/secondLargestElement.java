74c9bdd8d1197d5af57b5e279b8d9c2278e44c75
Rudresh <bamaniarudresh2008@gmail.com>
2026-03-25 20:45:17 +0530

    solve the second largest element in array

---
+++ Array/02_secondLargestElement/secondLargestElement.java
@@ -0,0 +1,21 @@
+public class secondLargestElement {
+    public static void main(String[] args){
+        int[] arr = {12,43,54,65,32};
+        int firstMax = -1;
+        int secondMax = -1;
+
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] > firstMax){
+                secondMax = firstMax;
+                firstMax = arr[i];
+            }
+            else if(arr[i] > secondMax && arr[i] != firstMax){
+                secondMax = arr[i];
+            }
+        }
+
+        System.out.println("Second largest element is: " + secondMax);
+
+    }
+
+}
