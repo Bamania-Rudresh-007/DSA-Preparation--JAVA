1e5a106545d6dc19bd532e86b438138f9a1fc4e5
Rudresh <bamaniarudresh2008@gmail.com>
2026-03-27 13:02:03 +0530

    done reverse the array

---
+++ Array/03_reverseArray/reverseArray.java
@@ -0,0 +1,25 @@
+public class reverseArray {
+    public static void main(String[] args){
+        int[] arr = {1,2,3,4,5,6};
+        int start = 0;
+        int end = arr.length-1;
+
+        System.out.print("Original Array:- ");
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i]);
+        }
+
+        while(start <= end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+
+        System.out.print("\nReversed Array:- ");
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i]);
+        }
+    }
+}
