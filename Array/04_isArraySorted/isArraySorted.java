3538d3b3baf25dab16a2d60f828c874f968ab7e1
Rudresh <bamaniarudresh2008@gmail.com>
2026-03-27 13:44:18 +0530

    done with isArraySorted

---
+++ Array/04_isArraySorted/isArraySorted.java
@@ -0,0 +1,21 @@
+public class isArraySorted {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        boolean flag = false;
+
+        for(int i = 0; i < arr.length - 1; i++){
+            if(arr[i] > arr[i + 1]){
+                flag = true;
+                break;
+            }
+        }
+
+        if(flag){
+            System.out.print("Array is not sorted...");
+        }
+        else{
+            System.out.print("Array is sorted");
+        }
+        
+    }    
+}
